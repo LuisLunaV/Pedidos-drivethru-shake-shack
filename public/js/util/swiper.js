9af22076ced8b78ac640d395582ee827d5d5063c
luislunav <luis.a.luna.v@outlook.com>
2023-11-05 23:52:03 -0600

    Se completa el apartado del carrucel de recomendaciones

--- public/js/util/swiper.js
+++ public/js/util/swiper.js
@@ -1,19 +1,34 @@
 export function carousel() {
   new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    direction: "horizontal",
-    loop: true,
-    autoplay: {
-      delay: 5000,
-    },
-    // effect: 'coverflow',
-    // coverflowEffect: {
-    //   rotate: 30,
-    //   slideShadows: false,
-    // }
-  
+    // slidesPerView: 3,
+    // spaceBetween: 25,
+    // direction: "horizontal",
+    // loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    // },
 
+// **---------------**//
+    // swiper-wrapper
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause: true,
+
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides:true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch:0,
+      depth:300,
+      modifier:1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
 
   });
 }
