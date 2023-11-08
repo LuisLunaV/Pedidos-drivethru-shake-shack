155b63637f8ab6852a9fc488eaadf8d84201c96a
luislunav <luis.a.luna.v@outlook.com>
2023-11-07 21:46:55 -0600

    Creamos un archivo para in mojor control de las importaciones y exportaciones de las funciones

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+import { addOrdersListHtml, addOrdersListCorrectHtml } from "./components/order-list.js";
+import { carousel } from "./util/swiper.js";
+
+export{
+    addOrdersListHtml,
+    addOrdersListCorrectHtml,
+    carousel
+}
