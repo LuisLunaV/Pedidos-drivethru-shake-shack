6467a760e79e29a822d986e53711727f94348edf
luislunav <luis.a.luna.v@outlook.com>
2023-11-07 21:50:26 -0600

    Depuramos el apartado de order list

---
+++ public/js/components/order-list.js
@@ -0,0 +1,43 @@
+const ulListOrders        = document.querySelector(".ulList-orders");
+const ulListOrdersCorrect = document.querySelector(".ulList-orders-correct");
+
+const addOrdersListHtml = ( data ) => {
+    
+  const html = `<li class="ulList-orders-li">
+                <div class="order-amount">
+                  <h2>${data.cantidad}</h2>
+                </div>
+                <div class="order-type">
+                  <h3>${data.description}</h3>
+                  <ul class="ul-order-content"></ul>
+                </div>
+                <div class="order-price">
+                 <span>$</span><p>${data.precio}</p>
+                </div>
+              </li>
+    `;
+
+
+    ulListOrders.innerHTML += html;
+};
+
+const addOrdersListCorrectHtml =( data )=>{
+    const html = `<li class="ulList-orders-li-correct">
+              <div class="order-amount-correct">
+                <h2>${data.cantidad}</h2>
+              </div>
+              <div class="order-type-correct">
+                <h3>${data.description}</h3>
+                <ul class="ul-order-content-correct"></ul>
+              </div>
+              <div class="order-price-correct">
+               <span>$</span><p>${data.precio}</p>
+              </div>
+            </li>`;
+            ulListOrdersCorrect.innerHTML += html;
+}
+
+export{
+    addOrdersListHtml,
+    addOrdersListCorrectHtml
+}
