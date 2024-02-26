4e13b6d862bbd92ae43ff8633fe2e063ec58876e
luislunav <luis.a.luna.v@outlook.com>
2024-02-26 11:45:03 -0600

    Se corrige el socket a uno

--- public/js/services/socket.cliente.js
+++ public/js/services/socket.cliente.js
@@ -2,7 +2,7 @@ import { printNewOrder } from "../index.js";
 export const socketsConnection=()=>{
 const socket = io();
 
-socket.on("imprimir-pedido-dos",( pedido )=>{
+socket.on("imprimir-pedido-uno",( pedido )=>{
     printNewOrder( pedido );
 });
 
